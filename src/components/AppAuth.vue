3d8577ac44ec366b563e2f7c7b60b1412effbd7e
Robin Blom <robin.blom@hotmail.se>
2023-03-04 14:55:13 +0100

    Added force-validation when user submits the form. Since we are using Vee-validate this will trigger any error messages if there are any and in that case not run the submit-function. If the form is filled in correctly we can access the values from the values-parameter thanks to Vee-validate.

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -81,7 +81,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -198,6 +198,11 @@ export default {
   computed: {
     ...mapState(useModalStore, ['hiddenClass']),
     ...mapWritableState(useModalStore, { modalVisibility: 'isOpen' }),
+  },
+  methods: {
+    register(values) {
+      console.log(values);
+    },
   },
 };
 </script>
