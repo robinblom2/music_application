e5e8a5bba5db4aafda6fa978cd20894fffd395d5
Robin Blom <robin.blom@hotmail.se>
2023-03-04 13:37:22 +0100

    Applied rules to our forms through a validation-schema, not to clutter each input field. In the validation-schema we supplied the name of the input-field and the rule we want to give the field. We also included the ErrorMessage-component in order to output error-messages if the input-fields fail validation.

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -81,7 +81,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -91,6 +91,7 @@
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <ErrorMessage class="text-red-600" name="name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
@@ -165,6 +166,15 @@ export default {
   data() {
     return {
       tab: 'login',
+      schema: {
+        name: 'required',
+        email: '',
+        age: '',
+        password: '',
+        confirm_password: '',
+        country: '',
+        tos: '',
+      },
     };
   },
   computed: {
