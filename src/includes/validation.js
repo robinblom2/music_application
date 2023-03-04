da8868b1ea1d3ea43a4cd4108b2e86e3734724a7
Robin Blom <robin.blom@hotmail.se>
2023-03-04 14:31:25 +0100

    Added validation for the password and confirm_password-inputs. Added the confirmed-rule which checks the field against another field to make sure that they match.

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -7,6 +7,7 @@ import {
   email,
   min_value as minVal,
   max_value as maxVal,
+  confirmed,
 } from '@vee-validate/rules';
 
 export default {
@@ -22,5 +23,6 @@ export default {
     defineRule('email', email);
     defineRule('min_value', minVal);
     defineRule('max_value', maxVal);
+    defineRule('confirmed', confirmed);
   },
 };
